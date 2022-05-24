6f005d1c3cf1b5e5760a154d805aa90dcb348777
R Vishnu Vardhan Reddy <36665976+VishnuVardhanReddyR@users.noreply.github.com>
2022-05-24 17:58:28 +0530

    initial commit
    initial layout

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './index.html',
+    './about.html',
+    './products.html',
+    './services.html',
+    './contact.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
